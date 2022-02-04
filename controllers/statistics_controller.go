16efcd41718496d86303fd04aecb06855e55fb33
Emmanuel <fortin.emmanuel@gmail.com>
2022-02-04 13:45:14 +0100

    Add cache for stats

--- controllers/statistics_controller.go
+++ controllers/statistics_controller.go
@@ -2,26 +2,34 @@ package controllers
 
 import (
 	"context"
+	"github.com/gin-gonic/gin"
+	"github.com/patrickmn/go-cache"
 	"github.com/rs/zerolog/log"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"hackathon-api/configs"
 	"hackathon-api/models"
 	"hackathon-api/responses"
 	"net/http"
 	"time"
-
-	"github.com/gin-gonic/gin"
-	"go.mongodb.org/mongo-driver/bson"
 )
 
 //https://www.mongodb.com/blog/post/quick-start-golang--mongodb--data-aggregation-pipeline
 
 var statsCollection = configs.GetCollection(configs.DB, "donations")
+var statsCache = cache.New(1*time.Minute, 2*time.Minute)
 
 func SumDonationsByMoney() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 		defer cancel()
+
+		stats, found := statsCache.Get("stats")
+
+		if found {
+			c.JSON(http.StatusFound, stats)
+			return
+		}
 
 		groupStage := bson.D{{"$group", bson.D{{"_id", "$moneyType"}, {"total", bson.D{{"$sum", "$amount"}}}}}}
 		resultCursor, err := statsCollection.Aggregate(ctx, mongo.Pipeline{ /*matchStage,*/ groupStage})
@@ -58,6 +66,12 @@ func SumDonationsByMoney() gin.HandlerFunc {
 			c.JSON(http.StatusInternalServerError, responses.DonationResponse{Status: http.StatusInternalServerError, Message: "error", Data: map[string]interface{}{"data": err.Error()}})
 			return
 		}
+
+		statsCache.Set("stats", StatResponse{
+			Stats: resultData,
+			Total: total,
+			Count: count,
+		}, 30*time.Second)
 
 		c.JSON(http.StatusOK, StatResponse{
 			Stats: resultData,
